c2a7011d7e882d216189396c8236943b7bd58ce0
AniketSharma-dev <aniketsharma.xo@gmail.com>
2024-10-16 20:05:00 +0530

    Added file

--- src/App.jsx
+++ src/App.jsx
@@ -6,67 +6,70 @@ import Footer from "./components/Footer/Footer";
 import Card from "./components/Card/Card";
 
 function App() {
-
-
-  const people = [
+  const users = [
     {
       name: "Arina Doe",
-      image: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      image:
+        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       profession: "Software Engineer",
       city: "New York",
       age: 28,
     },
     {
       name: "David Wilson",
-      image: "https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      image:
+        "https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       profession: "Graphic Designer",
       city: "San Francisco",
       age: 32,
     },
     {
       name: "Michael Brown",
-      image: "https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      image:
+        "https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       profession: "Data Analyst",
       city: "Chicago",
       age: 25,
     },
     {
       name: "Emily Davis",
-      image: "https://images.unsplash.com/photo-1517630800677-932d836ab680?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dhttps://unsplash.com/photos/closeup-photo-of-woman-beside-red-painted-wall-g0pb9aXpbgQ",
+      image:
+        "https://images.unsplash.com/photo-1517630800677-932d836ab680?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dhttps://unsplash.com/photos/closeup-photo-of-woman-beside-red-painted-wall-g0pb9aXpbgQ",
       profession: "Marketing Specialist",
       city: "Los Angeles",
       age: 30,
     },
     {
       name: "Jane Smith",
-      image: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      image:
+        "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       profession: "UX/UI Designer",
       city: "Seattle",
       age: 27,
     },
   ];
 
-
-  people.forEach((e)=>{
-    console.log(e);
-    
-  })
-
-
-  // console.log(people);
-  // console.log(people[0].name);
-  
-
+  // console.log(users);
+  // console.log(users[0].name);
 
   return (
     <>
       <Navbar />
 
-      
-      <Card img={people[0].image} userName={people[0].name} profession={people[0].profession} city={people[0].city} age={people[0].age}   />
-
-  
-      
+      <section className="w-full grid grid-cols-4 gap-7 px-20 py-10">
+        {users.map(function (e, ind) {
+          return (
+            <Card
+              key={ind}
+              img={e.image}
+              userName={e.name}
+              profession={e.profession}
+              city={e.city}
+              age={e.age}
+            />
+          );
+        })}
+      </section>
       <Form />
       <Footer />
     </>
